d39b062d77e40b5aca5354472609cd1c849c2242
actionpterygii <actionpterygii@gmail.com>
2019-02-15 00:55:25 +0900

    jsonから読み込んで出るということをね

--- popup.js
+++ popup.js
@@ -3,6 +3,19 @@ const result = document.getElementById('result');
 const usageBtn = document.getElementById('usageBtn');
 const readme = document.getElementById('readme');
 
+const jishoPath = 'jisho.json';
+var jisho = {};
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.extension.getURL(jishoPath), true);
+xhr.onreadystatechange = function()
+{
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+    {
+        jisho = JSON.parse(xhr.responseText);
+    }
+};
+xhr.send();
+
 window.onload = function()
 {
     input.focus();
@@ -15,17 +28,19 @@ input.onblur = function()
 
 input.onkeyup = function()
 {
-    result.innerHTML = input.value;
+    let value = input.value;
+
+    result.innerHTML = jisho[0]['kwsk'];
 };
 
 usageBtn.onclick = function()
 {
-    if(readme.style.display == "none")
+    if(readme.style.display == 'none')
     {
-        readme.style.display = "block";
+        readme.style.display = 'block';
     }
     else
     {
-        readme.style.display = "none";
+        readme.style.display = 'none';
     }
 };
