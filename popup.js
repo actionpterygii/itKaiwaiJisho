cc4a92745a9c4a68fda37292715562ff9abf0b1d
actionpterygii <actionpterygii@gmail.com>
2019-03-10 21:45:01 +0900

    こうかも(命名なんて知ーらない)

--- popup.js
+++ popup.js
@@ -3,64 +3,22 @@ const result = document.getElementById('result');
 const usageBtn = document.getElementById('usageBtn');
 const readme = document.getElementById('readme');
 
-// const jishoPath = 'jisho.json';
-// var jisho = {};
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', chrome.extension.getURL(jishoPath), true);
-// xhr.onreadystatechange = function()
-// {
-//     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-//     {
-//         jisho = JSON.parse(xhr.responseText);
-//     }
-// };
-// xhr.send();
-// class Jisho
-// {
-//     constructor (jishoPath)
-//     {
-//         this.all = {};
-//         this.xhr = new XMLHttpRequest();
-//         xhr.open('GET', chrome.extension.getURL(jishoPath), true);
-//         xhr.onreadystatechange = function()
-//         {
-//             if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-//             {
-//                 all = JSON.parse(xhr.responseText);
-//             }
-//         };
-//         xhr.send();
-//         this.aaa = 'fff';
-//     }
-
-//     get all()
-//     {
-//         return this.all;
-//     }
-//     get aaa()
-//     {
-//         return this.aaa;
-//     }
-
-//     // createResult(value)
-//     // {
-
-//     // }
-// }
-
-// class Jisho {
-//     constructor (aa) {
-//         this.aa = aa;
-//     }
-// }
-
-
-var jisho = 0;
+const jishoPath = 'jisho.json';
+var jisho = {};
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.extension.getURL(jishoPath), true);
+xhr.onreadystatechange = function()
+{
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+    {
+        jisho = JSON.parse(xhr.responseText);
+    }
+};
+xhr.send();
 
 window.onload = function()
 {
     input.focus();
-    jisho = new Jisho('jisho.json');
 };
 
 input.onblur = function()
@@ -71,9 +29,8 @@ input.onblur = function()
 input.onkeyup = function()
 {
     let value = input.value;
-    
 
-    result.innerHTML = jisho.aaa;
+    result.innerHTML = createResult(value);
 };
 
 usageBtn.onclick = function()
---
+++ result.js
@@ -0,0 +1,34 @@
+class Result
+{
+    constructor(jisho,value)
+    {
+        this.jisho = jisho;
+        this.value = value;
+        createResult();
+    }
+
+    createResult()
+    {
+        serch();
+        requiredElements.forEach(function(element)
+        {
+            this.entity += createHtml(element);
+        });
+    }
+
+    serch()
+    {
+
+        this.requiredElements = 000;
+    }
+
+    createHtml(element)
+    {
+        this.html = 0;
+        element.forEach(function(item)
+        {
+
+        });
+        return html;
+    }
+}
