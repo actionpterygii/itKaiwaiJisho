02ed5604c104b8c07bfe5b0fb816260449f342c5
actionpterygii <actionpterygii@gmail.com>
2019-03-13 01:24:52 +0900

    こういうね

--- result.js
+++ result.js
@@ -18,17 +18,55 @@ class Result
 
     serch()
     {
+        if(this.inputValue === '--all')
+        {
+            
+        }
 
         this.requiredElements = 'xxx';
     }
 
     createHtml(element)
     {
-        this.html = 0;
+        this.html = '<div class="tango">';
         for(let key in element)
         {
-
+            if(element[key] !== null)
+            {
+                switch(key)
+                {
+                    case kotb:
+                        this.html += 
+                            '<h2>' + element[key] + '</h2>';
+                        break;
+                    case eigo:
+                        this.html +=
+                            '<p class="eigo">' + element[key] + '</p>';
+                        break;
+                    case kwsk:
+                        this.html +=
+                            '<p class="kwsk">' + element[key] + '</p>';
+                        break;
+                    case tnjt:
+                        this.html +=
+                            '<dl class="tnjt">' +
+                                '<dt>転じて</dt>' +
+                                '<dd>' + element[key] + '</dd>' +
+                            '</dl>';
+                        break;
+                    case tigg:
+                        this.html +=
+                            '<dl class="tigg">' +
+                                '<dt>対義語</dt>' +
+                                '<dd>' + element[key] + '</dd>' +
+                            '</dl>';
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+        this.html += '</div>';
         return html;
     }
 }
