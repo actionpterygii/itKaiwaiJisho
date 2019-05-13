7f40ac93c27ebc7b95dd7994c8f559f90312bb81
actionpterygii <actionpterygii@gmail.com>
2019-05-14 02:56:34 +0900

    辞書は外に出す,devじゃなく,あとでgulpちゃんとする

--- jishoJsonWoSort.py
+++ jishoJsonWoSort.py
@@ -3,14 +3,18 @@
 import json
 import collections
 
-# 開発用ディレクトリ(src)から`jisho.json`もってくる
+# 辞書用ディレクトリ(jisho)から`jisho.json`もってくる
 # json.loadの2個目の引数は各項目内の順番ちゃんとするため
-with open('src/jisho.json') as f:
+with open('jisho/jisho.json') as f:
     df = json.load(f, object_pairs_hook=collections.OrderedDict)
 
 # kotbキーでソートする(破壊的に)
 df.sort(key=lambda x: x['kotb'])
 
-# 配布用ディレクトリ(dist)に`jisho.json`を書き込み(破壊的に)
-with open('dist/jisho.json', 'w+') as f:
+# 公開用ディレクトリ(docs)に`jisho.json`を書き込み(破壊的に)
+with open('/docs.json', 'w+') as f:
+    json.dump(df, f)
+
+# ChromeEctension用ディレクトリ(dist)に`jisho.json`を書き込み(破壊的に)
+with open('/ChromeExtension/dist/docs.json', 'w+') as f:
     json.dump(df, f)
