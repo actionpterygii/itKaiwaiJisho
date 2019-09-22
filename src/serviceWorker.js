30a6c497ab476684e743c916e6e3d92c7b667cd0
actionpterygii <actionpterygii@gmail.com>
2019-09-22 16:52:59 +0900

    けえし

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -14,10 +14,12 @@ self.addEventListener('install', function(event)
 {
     event.waitUntil(caches.open(CACHE_NAME).then(function(cache)
     {
-        return cache.addAll(urlsToCache.map(function(url)
-        {
-            new Request(url, {credentials: 'same-origin'})
-        }));
+        return cache.addAll(urlsToCache
+        //     .map(function(url)
+        // {
+        //     new Request(url, {credentials: 'same-origin'})
+        // })
+        );
     }));
 });
 
