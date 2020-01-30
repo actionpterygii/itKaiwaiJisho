fd8b004a4f055d66b8079023792cb755c058050c
actionpterygii <actionpterygii@gmail.com>
2020-01-30 23:35:46 +0900

    なんか

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,11 +1,22 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'itKaiwaiJishoCaches';
 var urlsToCache = [
-    './index.html',
-    './style.css',
-    './script.js',
-    './jisho.json',
-    './images/favicon.ico'
+    'index.html',
+    'style.css',
+    'script.js',
+    'setServiceWorker.js',
+    'pwacompat.min.js',
+    'jisho.json',
+    '/images/favicon.ico',
+    '/images/hoshixxx_72.png',
+    '/images/hoshixxx_96.png',
+    '/images/hoshixxx_128.png',
+    '/images/hoshixxx_144.png',
+    '/images/hoshixxx_152.png',
+    '/images/hoshixxx_192.png',
+    '/images/hoshixxx_384.png',
+    '/images/hoshixxx_512.png',
+    '/images/qrcode_1025.png'
 ];
 
 // インストール処理
