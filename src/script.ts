de48cabd8c7267604f5ca5a2997ff9d9b87b0115
actionpterygii <actionpterygii@gmail.com>
2020-02-01 22:56:13 +0900

    ダークモードうぇい

--- src/script.ts
+++ src/script.ts
@@ -232,14 +232,18 @@ function createHtml(element: tango): HTMLString
             // そのkeyはclassになんねん
             switch (key)
             {
-                // 言葉はh2要素
+                // 言葉はh2要素。絶対ある前提
                 case 'kotb':
                     html += 
-                        '<h2 class="' + key + '">' + element[key] + '</h2>';
+                        '<div class="tango_head"><h2 class="' + key + '">' + element[key] + '</h2>';
                     break;
+                // 絶対ある前提
+                case 'kwsk':
+                    html +=
+                        '</div><p class="' + key + '">' + element[key] + '</p>';
+                break;
                 // だいたいp要素
                 case 'eigo':
-                case 'kwsk':
                 case 'btmi':
                 case 'mnim':
                 case 'tigg':
